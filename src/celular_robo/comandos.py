3c4e71a6c07115b48f1b0a3aa5b569332a7add33
Ygor Matos <ygorsilvamatos@hotmail.com>
2026-09-13 18:32:17 -0300

    adicionando comandos e estrategias respeitando os TODOS

--- src/celular_robo/comandos.py
+++ src/celular_robo/comandos.py
@@ -7,3 +7,43 @@
 # TODO: implemente aqui. ComandoColeta(Comando): __init__(codinome, posicao,
 # quantidade), com .executar(robo) e .desfazer(robo) (remove o item da
 # bandeja, decrementa a contagem coletada).
+
+
+from celular_robo.comandos_base import Comando
+
+
+class ComandoColeta(Comando):
+    def __init__(self,codinome, posicao,quantidade ):
+        super().__init__()
+        self.codinome = codinome
+        self.posicao = posicao
+        self.quantidade = quantidade
+
+
+    def executar(self, robo):
+        if robo.posicao != self.posicao:
+            raise ValueError(
+                f"Robo esta em {robo.posicao}, "
+                f"mas o item esta em {self.posicao}"
+            )
+        robo.coletar(self.codinome, self.quantidade)
+        robo._historico_comandos.append(self)
+
+    
+    def desfazer(self, robo):
+        robo.desfazer_coleta(
+            self.codinome,
+            self.quantidade
+        )
+        
+        if robo._historico_comandos and robo._historico_comandos[-1] is self:
+            robo._historico_comandos.pop()
+
+    def __repr__(self):
+        return (
+            f"ComandoColeta("
+            f"{self.codinome!r}, "
+            f"{self.posicao!r}, "
+            f"{self.quantidade})"
+        )
+
--- src/celular_robo/estrategias.py
+++ src/celular_robo/estrategias.py
@@ -6,3 +6,54 @@
 # TODO: implemente aqui. Considere uma base comum (RotaColeta) com
 # __init_subclass__ registrando cada rota, ver Seção 2.2 (metaprogramação
 # aplicada a uma segunda hierarquia).
+
+
+from abc import ABC, abstractmethod
+
+from celular_robo.robo_base import Direcao
+
+class RotaColeta(ABC):
+    _registro_rotas = {}
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        RotaColeta._registro_rotas[cls.__name__] = cls
+
+    @abstractmethod
+    def mover(self, robo, localizacaoItem):
+        ...
+
+class RotaDireta(RotaColeta):
+    def executar(self, robo, localizacaoItem):
+        destino_x, destino_y = localizacaoItem
+
+        atual_x, atual_y = robo.posicao
+
+        if destino_x > atual_x:
+            robo.girar_ate(Direcao.LESTE)
+            robo.avancar_n(destino_x - atual_x)
+
+        elif destino_x < atual_x:
+            robo.girar_ate(Direcao.OESTE)
+            robo.avancar_n(atual_x - destino_x)
+        
+        atual_y = robo.y
+
+        if destino_y > atual_y:
+            robo.girar_ate(Direcao.NORTE)
+            robo.avancar_n(destino_y - atual_y)
+
+        elif destino_y < atual_y:
+            robo.girar_ate(Direcao.SUL)
+            robo.avancar_n(atual_y - destino_y)
+        #aqui eu realizaria robo.coletar
+        return robo.posicao
+
+
+class RotaComDuplaConferencia(RotaColeta):
+    pass
+    #ainda não entendi muito bem como implementar rotacomduplaconferencia, pois ainda não sei se
+    #  vou usar robo.coletar dentro do método, vou implementar
+    #  essa estrategia depois
+
+ESTRATEGIAS_VALIDAS = set(RotaColeta._registro_rotas)
